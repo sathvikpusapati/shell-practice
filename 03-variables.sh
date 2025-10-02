46a9b9be32c8d78751b932494f649391542370b4
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-02 22:49:30 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# no space b/w name and equal
+
+person1=sathvik
+person2=sravani
+
+echo "$person1 : hi"
+echo "$person2 : hi $person1 , how are you"
+echo "$person1 :fine man what about you"
+echo "$person2 : me too "
