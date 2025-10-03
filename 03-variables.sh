fe59131d97b7c9afd8fc4925653ed77bde529b33
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-04 00:41:22 +0530

    small convo update

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,9 +3,11 @@
 # no space b/w name and equal
 
 person1=sathvik
-person2=sravani
+person2=pavan
 
 echo "$person1 : hi"
 echo "$person2 : hi $person1 , how are you"
 echo "$person1 :fine man what about you"
 echo "$person2 : me too "
+echo "$person1 : do you have gf"
+echo "$person2 : no i don't"
