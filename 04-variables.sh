b117fbdf90683a756e3a2d5a2bf3852ebe0953a3
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-04 02:18:19 +0530

    small convo update

---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Arguments
+
+person1=$1
+person2=$2
+
+echo "$person1 : welcome to alaska $person2"
+echo "$person2 : nice meeting you $person1 , how are you"
+echo "$person1 :fine $person2 what about you"
+echo "$person2 : i am doing great $person1  "
+echo "$person1 : cool , $person2 can we discuss about crypto currency"
+echo "$person2 : yes $person1"
+echo "$person1 : thank you $person2"
