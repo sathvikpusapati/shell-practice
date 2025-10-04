e4f54e16c1acc9d2043f15ef7e720d902fc3273c
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-04 15:33:43 +0530

    env variables

--- 06-env-variables.sh
+++ 06-env-variables.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "course name is :$COURSE"
+
+# in server home location wwe have .bashrc file in that we need to add 
+# COURSE=" name " before the last line and run source .bashrc command 
+# this will make the variable to stay even after the process is complete 
+# that means after exit also we can have that value
