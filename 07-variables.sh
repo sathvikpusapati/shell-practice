dba4a1d6036ef21283de1150ded4209c296a9f88
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-04 16:05:11 +0530

    env variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -4,3 +4,12 @@ DATE=$(date) #this will run the date command and store in DATE variable
 
 echo "Timestamp executed :$DATE"
 
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "process done in $TOTAL_TIME"
