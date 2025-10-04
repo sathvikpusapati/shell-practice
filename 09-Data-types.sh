ade560f0762a8a78f1592da4acf733cb740e16cf
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-04 18:15:56 +0530

    data types

--- 09-Data-types.sh
+++ 09-Data-types.sh
@@ -6,7 +6,19 @@ NUMBER2=100
 
 NAME=sathvik
 
+#even if we give wrong it wont throws an error
 sum=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "sum is $sum"
+
+LEADERS=("putin" "modi" "obama" "trump")
+
+echo "leaders are : ${LEADERS[@]}"
+
+echo "leader at 1st index is : ${LEADERS[1]}"
+
+echo "5th leader : ${LEADERS[6]}"
+#it wont throw any out of bound error like other programs
+
+
 
