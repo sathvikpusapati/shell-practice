d7b449338af58fff5239e376003b39ea833a4bbb
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-05 19:44:33 +0530

    data types

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,7 +6,7 @@ if [ $number -lt 10 ]; then
 
     echo "given number $number is less tham 10"
 
-else [ $number -eq 10 ]; then
+elif [ $number -eq 10 ]; then
 
     echo "given nummber $number is equal to 10"
 
