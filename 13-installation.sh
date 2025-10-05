ea1549da1b904f8f28065c8553412bc8fa7727a2
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-05 21:04:15 +0530

    data types

---
+++ 13-installation.sh
@@ -0,0 +1,31 @@
+#!/bin/baash
+
+number=$(id -u)
+
+if[ $number -ne 0 ]; then
+
+    echo "ERROR : provide root privalages to run the script"
+    exit 1
+fi
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]; then
+
+        echo "ERROR : $2 installation is FAILURE"
+
+    else
+
+        echo "$2 installation is SUCCESS"
+
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nodejs -y
+VALIDATE $? "NODEJS"
+
+dnf install nginx -y
+VALIDATE $? "NGINX"
