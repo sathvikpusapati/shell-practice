bf37f0734c15add4a4216dc86e2c330531c7ceaf
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-05 20:40:18 +0530

    data types

--- 12-installation.sh
+++ 12-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-number=$(id -u)
+number=$(id -u) #only root  user id is 0 so we are checking that
 
 if [ $number -ne 0 ]; then
 
@@ -10,7 +10,7 @@ fi
 
 dnf install mysql -y
 
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then # $? exit status of last command is saved here if 0 success if 1 failure
 
     echo "mysql installation is succes"
 
