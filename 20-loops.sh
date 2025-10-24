287a5393af1c58e97a26f6feab4465d2b86fff45
pusapati sathvik <pusapatisathvik@gmail.com>
2025-10-24 21:12:37 +0530

    loops

--- 20-loops.sh
+++ 20-loops.sh
@@ -12,7 +12,7 @@ name=$( echo $0 | cut -d "." -f1)
 
 logfile=$folder/$name.log
 
- mkdir -p $folder &>> $logfile
+sudo mkdir -p $folder 
 
 if [ id -ne 0 ]; then
     echo -e "$R please give root access to continue$N" | tee -a $logfile
