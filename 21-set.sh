cae787378db445e52dbdf072673376e2db35ee6b
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-09 17:41:04 +0530

    sample

--- 21-set.sh
+++ 21-set.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-trap 'echo "there is an error in $LINENO and the command is $BASH_COMMAND"' ERR
+trap 'echo "there is an error in line number  $LINENO and the command is $BASH_COMMAND"' ERR
 
 echo "before error"
 
---
+++ 23-script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "my country is $name"
+
+echo "PID OF SCRIPT-2 $$"
+
