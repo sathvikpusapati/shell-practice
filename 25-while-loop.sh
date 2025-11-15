c31d449a65afc1443bcf20d3f6277396b5ef5cfa
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-15 22:10:59 +0530

    all

--- 25-while-loop.sh
+++ 25-while-loop.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-count=5
+#count=5
 
-echo "starting countdown...."
+##echo "starting countdown...."
 
-while [ $count -gt 0 ]
-do
-    echo "time left : $count"
-    sleep 1
-    count=$((count - 1))
-done
+#while [ $count -gt 0 ]
+#do
+#   sleep 1
+#    count=$((count - 1))
+#done
 
-echo "Times Up !"
+#echo "Times Up !"
+
+while IFS= read -r  line ; do
+
+    echo "processing line : $line"
+
+done < 22-script-1.sh
