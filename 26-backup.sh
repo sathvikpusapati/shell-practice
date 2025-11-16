7cc3bd85bb17fdbf9466204e75d907d7864f79b2
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-16 22:01:06 +0530

    all

--- 26-backup.sh
+++ 26-backup.sh
@@ -8,7 +8,7 @@ id=$(id -u)
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-
+DAYS=${3:-14} #if not provided consider as 14 days
 folder="/var/log/shell-script"
 
 name=$( echo $0 | cut -d "." -f1)
@@ -40,4 +40,16 @@ fi
 if [ ! -d $DEST_DIR ]; then
     echo -e "$R DEstination $DEST_DIR does not exist$N"
     exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -type f -name "*.log" -mtime +$DAYS)
+
+if [ ! -z "${FILES}" ]; then
+    echo "FILES found $FILES"
+    TIMESTAMP=$(date +%F-%H-%M)
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    echo "ZIP FILE NAME : $ZIP_FILE_NAME"
+    echo $FILES | zip -@ -j "$ZIP_FILE_NAME"
+else
+    echo -e "files not found $YSKIPPING $N"
 fi
