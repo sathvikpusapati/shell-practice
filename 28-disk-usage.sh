4a40fe7553161eda72716a56d32668d32ef22ebe
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-22 17:41:41 +0530

    auto

--- 28-disk-usage.sh
+++ 28-disk-usage.sh
@@ -2,12 +2,15 @@
 
 DISK_USAGE=$( df -hT | grep -v Filesystem )
 DISK_THRESHOLD=2 # in companies and projects the  threshold value is 75
-
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read -r line 
 do
     USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ] ; then
-        echo "HIGH USAGE ON $PARTITION : $USAGE"
+        MESSAGE="HIGH USAGE ON $PARTITION : $USAGE %"
     fi
 done <<< $DISK_USAGE
+
+echo "message body : $MESSAGE"
+
