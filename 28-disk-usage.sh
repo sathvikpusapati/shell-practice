55ee031ca918e8d604f08d4753f640c42f0a717d
pusapati sathvik <pusapatisathvik@gmail.com>
2025-11-22 17:34:12 +0530

    auto

--- 28-disk-usage.sh
+++ 28-disk-usage.sh
@@ -5,11 +5,9 @@ DISK_THRESHOLD=2 # in companies and projects the  threshold value is 75
 
 while IFS= read -r line 
 do
- 
     USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
-
     if [ $USAGE -ge $DISK_THRESHOLD]; then
-        echo "HIGH USAGE ON $PARTITION $USAGE"
+        echo "HIGH USAGE ON $PARTITION : $USAGE"
     fi
 done <<< $DISK_USAGE
